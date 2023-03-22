256713de97cb40017749acca4d6995399987d995
Kuba Seliga <57732056+kseligga@users.noreply.github.com>
2023-03-22 01:06:49 +0100

    wektoryzacja & wykrywanie jezyka

--- fake_news_ml.py
+++ fake_news_ml.py
@@ -1,28 +1,30 @@
-#%%
+# %%
 import pandas as pd
 import numpy as np
-#import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import warnings
+
+from langdetect import detect
 from matplotlib import pyplot as plt
 import contractions
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
+from numpy import hstack
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 import seaborn as sns
 
-
-nltk.download('stopwords')
-
+# nltk.download('stopwords')
 
 warnings.filterwarnings('ignore')
 np.random.seed = 42
 
 df = pd.read_csv('PreProcessedData.csv')
-#print(df.head())
+# print(df.head())
 
-#print(data.loc[0:3, "title"])
+# print(data.loc[0:3, "title"])
 
 # removing id number from df
 df = df.drop(df.columns[0], axis=1)
@@ -30,11 +32,9 @@ df = df.drop(df.columns[0], axis=1)
 # dropping duplicated rows (9600/69045)
 df = df.drop_duplicates()
 
-
-
 # print(df.head())
 # print(len(df['text'].unique()))
-#print((df[df['text'].duplicated(keep=False)])["text"])
+# print((df[df['text'].duplicated(keep=False)])["text"])
 
 # there are 9 instances where same news is labelled both as true and false
 # print(df[df.duplicated(subset=['title', 'text'], keep=False)])
@@ -42,9 +42,9 @@ df = df.drop_duplicates()
 # there aren't any cases where both are NaN
 # print(df.loc[df['text'].isna() & df['title'].isna()])
 
-df["Ground Label"]=np.where(df["Ground Label"]=="true",1,0)
-df['text']=df['text'].astype('string')
-df['title']=df['title'].astype('string')
+df["Ground Label"] = np.where(df["Ground Label"] == "true", 1, 0)
+df['text'] = df['text'].astype('string')
+df['title'] = df['title'].astype('string')
 
 na_ratio_cols = df.isna().mean(axis=0)
 print(na_ratio_cols)
@@ -52,15 +52,34 @@ print(na_ratio_cols)
 df.info()
 df.hist(column='Ground Label')
 
+
+# wykrywanie języka - bo wszystkie nie-angielskie to fake (767/59445) - być może one do wywalenia (?)
+def is_english(text):
+    try:
+        lang = detect(text)
+    except:
+        return False
+    return lang == 'en'
+
+df['is_english'] = df['text'].apply(lambda x: is_english(x))
+
+
 # filling empty and na spaces
 def changeNA(text):
     if not isinstance(text, str) or pd.isnull(text) or len(text) < 3:
         return "brak danych"
     return text
 
+
 df['title'] = df['title'].apply(lambda x: changeNA(x))
 df['text'] = df['text'].apply(lambda x: changeNA(x))
-#%%
+
+print('NA changed')
+
+
+
+
+# %%
 
 # contractions
 # zamienia skróty na pełne słowa (np: u - you, don't - do not)
@@ -72,25 +91,33 @@ def remove_contractions(text):
         s = text
     return s
 
+
 df['title'] = df['title'].apply(lambda x: remove_contractions(x))
 df['text'] = df['text'].apply(lambda x: remove_contractions(x))
-#%%
+
+print('contractions removed')
+# %%
 
 
 # stop words
 # usuwa słowa nie noszące informacji same w sobie, np zdanie "Donald Trump is being under control of police" zamieni na "Donald Trump under control police"
 stop_words = set(stopwords.words('english'))
 stop_words.add('http')
+
+
 def remove_stopwords(text):
     return " ".join([word for word in str(text).split() if word not in stop_words])
+
 
 df['title'] = df['title'].apply(lambda x: remove_stopwords(x))
 df['text'] = df['text'].apply(lambda x: remove_stopwords(x))
 
+print('stopwords removed')
 
-#stemming
+# stemming
 # zamienia słowa w ich podstawę bez końcówek
 stemmer = PorterStemmer()
+
 def stem_words(text):
     return " ".join([stemmer.stem(word) for word in text.split()])
 
@@ -99,12 +126,15 @@ df['title'] = df['title'].apply(lambda x: stem_words(x))
 # takes some time:
 df["text"] = df["text"].apply(lambda x: stem_words(x))
 
-#print(df['title'].head())
+print("stemming done")
+# print(df['title'].head())
 
-#tokenizacja
-#Tworzymy nową kolumnę z tablicą słów użytych w tekscie, bo komp i tak nie rozumie zdan tylko se ogarnia gdzie byly jakie slowa uzywane z jakimi innymi slwoami
-df['tokenised_text'] = df['text'].apply(lambda x: word_tokenize(x))
-df['tokenised_title'] = df['title'].apply(lambda x: word_tokenize(x))
+# tokenizacja
+# Tworzymy nową kolumnę z tablicą słów użytych w tekscie, bo komp i tak nie rozumie zdan tylko se ogarnia gdzie byly jakie slowa uzywane z jakimi innymi slwoami
+df['tokenized_text'] = df['text'].apply(lambda x: word_tokenize(x))
+df['tokenized_title'] = df['title'].apply(lambda x: word_tokenize(x))
+
+print("tokenization done")
 
 def count_proper_nouns(token_text):
     proper_nouns_counter = 0
@@ -112,27 +142,38 @@ def count_proper_nouns(token_text):
     for i in range(len(tagged)):
         word, pos = tagged[i]
         if pos == 'NNP':
-            if i!=0 and not tagged[i-1][1] in ['.', '!', '?']:
-                proper_nouns_counter+=1
+            if i != 0 and not tagged[i - 1][1] in ['.', '!', '?']:
+                proper_nouns_counter += 1
     return proper_nouns_counter
-#Zliczanie nazw wlasnych - zajmuje duzo czasu
+
+
+# Zliczanie nazw wlasnych - zajmuje duzo czasu
 df['words_counter'] = df['text'].apply(lambda x: len(x.split()))
-                
-df['proper_nouns_counter'] = df['tokenised_text'].apply(lambda x: count_proper_nouns(x))/df['words_counter']
-df['coma_counter'] = df['tokenised_text'].apply(lambda arr: len(list(filter(lambda x: x == ',', arr))))/df['words_counter']
-df['exclamation_mark_counter'] = df['tokenised_text'].apply(lambda arr: len(list(filter(lambda x: x == '!', arr))))/df['words_counter']
-df['question_mark_counter'] = df['tokenised_text'].apply(lambda arr: len(list(filter(lambda x: x == '?', arr))))/df['words_counter']
-#%%
+
+df['proper_nouns_counter'] = df['tokenized_text'].apply(lambda x: count_proper_nouns(x)) / df['words_counter']
+df['coma_counter'] = df['tokenized_text'].apply(lambda arr: len(list(filter(lambda x: x == ',', arr)))) / df[
+    'words_counter']
+df['exclamation_mark_counter'] = df['tokenized_text'].apply(lambda arr: len(list(filter(lambda x: x == '!', arr)))) / \
+                                 df['words_counter']
+df['question_mark_counter'] = df['tokenized_text'].apply(lambda arr: len(list(filter(lambda x: x == '?', arr)))) / df[
+    'words_counter']
+
+print("word counting done")
+
+# %%
 
 
 # splitting data
 
-X_train, X_test = train_test_split(
-    df, test_size=0.3, random_state=42
+X = df.drop('Ground Label', axis=1)
+y = df['Ground Label']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, stratify=y, test_size=0.3, random_state=42
 )
 
-X_train, X_val = train_test_split(
-    X_train, test_size=0.3, random_state=42
+X_train, X_val, y_train, y_val = train_test_split(
+    X_train, y_train, test_size=0.3, random_state=42
 )
 
 # X_train - our training
@@ -149,4 +190,24 @@ print(na_ratio_cols)
 na_ratio_cols = X_val.isna().mean(axis=0)
 print(na_ratio_cols)
 
-corrMatrix=df.corr()
+corrMatrix = df.corr()
+
+
+# wektoryzacja - rozbicie tekstu na kolumny zliczające ilość wystąpień słów
+
+# X_stats_train = X_train.drop(['title', 'text', 'tokenized_text', 'tokenized_title'], axis=1)
+# X_stats_val = X_val.drop(['title', 'text', 'tokenized_text', 'tokenized_title'], axis=1)
+#
+# vectorizer_title = TfidfVectorizer()
+# Xv_title_train = vectorizer_title.fit_transform(X_train['title'])
+# Xv_title_val = vectorizer_title.transform(X_val['title'])
+#
+# vectorizer_text = TfidfVectorizer()
+# Xv_text_train = vectorizer_text.fit_transform(X_train['text'])
+# Xv_text_val = vectorizer_text.transform(X_val['text'])
+#
+# coś sie tu wykrzacza
+# X_train= hstack([X_stats_train, Xv_title_train, Xv_text_train])
+# X_test= hstack([X_stats_val, Xv_title_val, Xv_text_val])
+#
+# print(list(X_train.columns))
